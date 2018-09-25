2442a4fc9d737345863502e50aa6c215b51bb2a1
lionheartpc <lionheartpc@gmail.com>
2018-09-26 02:00:18 +0300

    Initial FIles

---
+++ LineParser/LineParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LineParser
+{
+    class LineParser
+    {
+
+        private string ReadFile(string path)
+        {
+            //Dont believe in pointless error handling, unless there is a need to hide/log errors. Keep it simple
+            string contents = System.IO.File.ReadAllText(@path);
+
+            return contents;
+        }
+
+        private void WriteFile(string path, string contents)
+        {
+            System.IO.File.WriteAllText(@path, contents);
+        }
+
+        private string RemoveLineBreaks(string content)
+        {
+            string formated = Regex.Replace(content, @"\r\n?|\n", ""); ;
+            return formated;
+        }
+
+        private string LimitCharacters(string content)
+        {
+            string returnValue = "";
+            return returnValue;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ LineParserTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LineParserTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LineParserTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("283933ca-7946-481a-baf4-19c90dfe4a5f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

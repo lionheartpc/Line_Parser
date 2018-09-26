e98b5f88bb95d9b3c830b4e890ef7913fad5ae7e
lionheartpc <lionheartpc@gmail.com>
2018-09-26 16:17:31 +0300

    Implemented initial LineParser method for wrapping text, added first failing test.

--- LineParser/LineParser.cs
+++ LineParser/LineParser.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
-namespace LineParser
+namespace LineParserClass
 {
-    class LineParser
+    public class LineParser
     {
-
-        private string ReadFile(string path)
+        /// <summary>
+        /// Reads file from a given location, if path does not exist returns an error
+        /// </summary>
+        /// <param name="path"> path to a file to be read</param>
+        /// <returns>contents of a string read from a file</returns>
+        public string ReadFile(string path)
         {
             //Dont believe in pointless error handling, unless there is a need to hide/log errors. Keep it simple
             string contents = System.IO.File.ReadAllText(@path);
@@ -18,25 +20,77 @@ namespace LineParser
             return contents;
         }
 
-        private void WriteFile(string path, string contents)
+        /// <summary>
+        /// Writes to a file at a location
+        /// </summary>
+        /// <param name="path"> path of a file to write on</param>
+        /// <param name="contents"> contents to be written to a file at a path location</param>
+        public void WriteFile(string path, string contents)
         {
             System.IO.File.WriteAllText(@path, contents);
         }
 
-        private string RemoveLineBreaks(string content)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public string RemoveLineBreaks(string content)
         {
-            string formated = Regex.Replace(content, @"\r\n?|\n", ""); ;
+            string formated = Regex.Replace(content, @"\r\n?|\n", " "); ;
             return formated;
         }
 
-        private string LimitCharacters(string content)
+        public string LimitCharacters(string content, int rowLength)
         {
-            string returnValue = "";
-            return returnValue;
+
+            string line = "";
+            StringBuilder result = new StringBuilder();
+
+            Queue<string> queue = new Queue<string>(content.Split(' '));
+
+            while (queue.Count > 0 )
+            {
+                var word = queue.Dequeue();
+
+
+                if (word.Length >= rowLength)
+                {
+                    result.Append(line + System.Environment.NewLine);
+                    line = "";
+                    result.Append(word + System.Environment.NewLine);
+                }
+                else
+                {
+                    if ((line + word).Length > rowLength-1)
+                    {
+                        result.Append(line + System.Environment.NewLine);
+                        line = word;
+                    }
+                    else
+                    {
+                        line =line.Trim() +" "+ word.Trim();
+                    }
+                }
+
+                if (queue.Count == 0)
+                {
+                    result.Append(line);
+                }
+
+
+            }
+            return result.ToString();
         }
 
         static void Main(string[] args)
         {
+            string content;
+            LineParser obj = new LineParser();
+            content = obj.ReadFile("D:\\c.txt");
+            content = obj.RemoveLineBreaks(content);
+            content = obj.LimitCharacters(content, 10);
+            obj.WriteFile("D:\\d.txt", content);
         }
     }
 }
---
+++ LineParserTests/LineParserTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LineParserClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineParserClass.Tests
+{
+    [TestClass()]
+    public class LineParserTests
+    {
+        [TestMethod()]
+        public void LimitCharactersTest()
+        {
+            //Arrange
+            var testObject = new LineParser();
+            string expectedResult = "Word Word";
+            string testData = "Word Word";
+            string actualResult;
+            //Act
+
+            actualResult = testObject.LimitCharacters(testData, 4);
+
+            //Assert
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+    }
+}
